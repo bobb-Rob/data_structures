29afdefda0b4a36d86ba3ef10fe1ac0a7f39ea8d
Robertson Akpan <robertsonakpan@gmail.com>
2023-03-31 10:56:24 +0100

    Cypres installed

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,1 +1,13 @@
 // Cypress Spec
+
+describe('First test Group', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('does not do much!', () => {
+    cy.get('#email').type('Robertsonakpan@gmail.com')
+    cy.get('#password').type('robert')
+    cy.get('.text-md').click()
+  })
+})
